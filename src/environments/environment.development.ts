b42e47d95d3b344ec2c5552d499fcbbb222fe514
suzkapu <simon-praher@gmx.at>
2026-06-02 20:46:25 +0200

    Change callback

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   spotifyUrl: 'https://api.spotify.com/v1',
-  appUrl: 'https://analytify.dynv6.net:4200',
+  appUrl: 'https://analytify.dynv6.net',
   authorizeUrl: 'https://accounts.spotify.com/authorize',
 };
