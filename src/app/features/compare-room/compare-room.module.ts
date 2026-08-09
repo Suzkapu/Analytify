f698b144c5395331b62616b1aea36bd738824eda
suzkapu <simon-praher@gmx.at>
2026-08-09 13:34:29 +0200

    Align compare room with app layout

--- src/app/features/compare-room/compare-room.module.ts
+++ src/app/features/compare-room/compare-room.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {LayoutModule} from '@shared/layout/layout.module';
 import {SharedModule} from '@shared/shared.module';
 import {CompareRoomCallbackComponent} from './compare-room-callback.component';
 import {CompareRoomJoinComponent} from './compare-room-join.component';
@@ -9,6 +10,7 @@ import {CompareRoomShellComponent} from './compare-room-shell.component';
   declarations: [CompareRoomShellComponent, CompareRoomJoinComponent, CompareRoomCallbackComponent],
   imports: [
     SharedModule,
+    LayoutModule,
     RouterModule.forChild([
       {path: 'callback', component: CompareRoomCallbackComponent},
       {path: 'join/:roomId', component: CompareRoomJoinComponent},
