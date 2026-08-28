d4eee45ea0b3fe654f5aed32c995f93d3c3f6382
suzkapu <simon-praher@gmx.at>
2026-08-28 23:46:14 +0200

    fix: initialize CI integration test adapters
    Refs #20

--- karma.supabase-integration.conf.cjs
+++ karma.supabase-integration.conf.cjs
@@ -1,12 +1,27 @@
 module.exports = function configureSupabaseIntegration(config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     client: {
+      clearContext: false,
       args: [
         '--analytify-supabase-integration',
         process.env.SUPABASE_INTEGRATION_URL || '',
         process.env.SUPABASE_INTEGRATION_ANON_KEY || ''
       ]
     },
+    reporters: ['progress', 'kjhtml'],
+    jasmineHtmlReporter: {suppressAll: true},
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: false,
     browserNoActivityTimeout: 60_000,
     captureTimeout: 60_000
   });
